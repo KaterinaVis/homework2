387e4df342c4462cd26b19627e8bde71490be999
Katerina <katerina4484@mail.ru>
2022-08-11 23:08:16 +0500

    ex13

---
+++ ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+int number = ReadInt("Введите число: ");
+
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (number > 99)
+{
+    int secondDigit = (number / 10) % 10;
+    Console.WriteLine(secondDigit);
+}
+
+
+
+int ReadInt(string message)  
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
