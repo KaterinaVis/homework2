75b58e10d116ebea5dc2a0b15d568b91d0a52a78
Katerina <katerina4484@mail.ru>
2022-08-11 22:41:31 +0500

    ex10m

---
+++ ex10m/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1        с ИСПОЛЬЗОВАНИЕМ МЕТОДОВ */ 
+
+int number = ReadInt("Введите трехзначное число: ");
+
+if(number > 99 && number < 1000)
+{
+    int secondDigit = (number / 10) % 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Ошибка! Вы ввели не трехзначное число");
+}
+
+
+
+int ReadInt(string message)  // метод конвертации из ввода в число
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
