586f76496d3ae38dda3d023c56f945df549197e6
Katerina <katerina4484@mail.ru>
2022-08-14 14:35:50 +0500

    ex13m

---
+++ ex13m/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 *  C ИСПОЛЬЗОВАНИЕМ МЕТОДА */
+
+
+string numbertext = ReadString("Введите число: ");
+
+if (numbertext.Length > 2)
+{
+ Console.Write("Третья цифра числа " + numbertext[2]);
+}
+else
+{
+    Console.Write("В этом числе третьей цифры нет");
+}
+
+
+string ReadString(string message)  
+{
+Console.Write(message);
+return Convert.ToString(Console.ReadLine());
+}
